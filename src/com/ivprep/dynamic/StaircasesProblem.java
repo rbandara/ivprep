9b26e108373cdb416c15113b097d1c6aeab44f8a
Ravindra Bandara <ravindra.bandara@gmail.com>
2015-06-28 00:03:15 -0400

    initial commit

---
+++ src/com/ivprep/dynamic/StaircasesProblem.java
@@ -0,0 +1,19 @@
+package com.ivprep.dynamic;
+
+/**
+ * A stair case can be climbed up with 1 steps or 2 steps at a time.
+ * <p/>
+ * What are the different ways the stairs can be climbed up?
+ * <p/>
+ * ex : 1 = {1}
+ * 2 = {1,1}, {2}
+ * 3 = {1,1,1}, {2,1}, {1,2}
+ * 4 = {1,1,1,1},{2,1,1},{1,2,1}, {1,1,2}, {2,2}
+ */
+public class StaircasesProblem {
+    // this is the fibonacci series
+    // f(n) = f(n-1)+f(n-2)
+
+    // if the problem states differently, such that 13 stairs and allowed to climb up 1,2,3,4 or 5
+
+}
