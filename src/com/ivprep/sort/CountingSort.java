5aff22df2bfac9d78e9ff12e90c0fc9f9cb5abf4
Ravindra Bandara <ravindra.bandara@gmail.com>
2015-07-10 01:09:17 -0400

    counting sort

--- src/com/ivprep/sort/CountingSort.java
+++ src/com/ivprep/sort/CountingSort.java
@@ -1,5 +1,14 @@
 package com.ivprep.sort;
 
+// One of the draw backs in counting sort is the maximum number of elements
+// that can be sorted has a upper bound of Integer.MAX_VALUE
+// This code will see the performance of counting sort for that MAX number of elements
+// The results are compared with the built in search algorithms
+// Note : Java collections use Merge sort while Arrays use quick sort ( variant of quick sort - DualPivotQuick sort )
+// this gives you why
+// http://stackoverflow.com/questions/15154158/why-collections-sort-uses-merge-sort-instead-of-quicksort
+
+
 public class CountingSort {
 
     static int MAX = Integer.MAX_VALUE;
